07ca65439cc711dda7b02ffccc8ae044c454d5d7
mdsam001 <sarimmohd22@gmail.com>
2024-08-19 23:04:14 +0530

    New Files added

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,13 +2,13 @@ import NavBar from "./NavBar"
 import PostSpace from "./PostSpace"
 import StoryCard from "./StoryCard"
 import StorySpace from "./StorySpace"
-
+import "../assets/Card.css"
 
 // eslint-disable-next-line react/prop-types
 const Card = ({ cardContainer }) => {
     return (
         <>
-            <div ref={cardContainer} className="w-[36%] h-[91%] bg-white rounded-2xl  overflow-hidden">
+            <div id="card" ref={cardContainer} className="w-[36%] h-[91%] bg-white rounded-2xl  overflow-hidden">
                 <StoryCard></StoryCard>
                 <NavBar />
                 <StorySpace />
