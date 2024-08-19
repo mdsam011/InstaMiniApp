ef25058d12d22e4fa944b2f5ba021e804e051f6d
mdsam001 <sarimmohd22@gmail.com>
2024-08-19 23:17:34 +0530

    New Files added

--- src/Components/Stories.jsx
+++ src/Components/Stories.jsx
@@ -5,7 +5,7 @@
 const Stories = ({ elem }) => {
   return (
     <>
-      <div className={`stories h-[80%] w-[85px] bg-blue-50  z-10 bg-cover shrink-0 overflow-hidden  rounded-[50%] border-2 border-orange-700`}>
+      <div id="stories" className={`stories h-[80%] w-[85px] bg-blue-50  z-10 bg-cover shrink-0 overflow-hidden  rounded-[50%] border-2 border-orange-700`}>
         <img id={elem.id} src={`${elem.dp}`} alt="" />
       </div>
 
